fdfd954a8b24cbf28fb2b2e501c9f9074f2eafc0
vvasuki <vishvas.vasuki@gmail.com>
2019-01-30 14:05:01 -0800

    auto-build some feeds with travis

--- build_feeds.sh
+++ build_feeds.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mkdir auto-built-feeds
 curl -X GET "https://scala.vedavaapi.org/podcasts/v1/archiveRequests?archiveRequestUri=https%3A%2F%2Fgithub.com%2Fsanskrit-coders%2Frss-feeds%2Fraw%2Fmaster%2Ffeeds%2Fsa%2FrequestJsons%2FmahAbhArata-mUla-paThanam.json" -H "accept: application/rss+xml" > auto-built-feeds/mahAbhArata-mUla-paThanam.rss
