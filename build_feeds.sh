498dd24e0b2754983a0bf1e6664237fb01bf22fb
vvasuki <vishvas.vasuki@gmail.com>
2019-02-05 17:29:17 -0800

    Content update

--- build_feeds.sh
+++ build_feeds.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
+
+## Setup
 mkdir auto-built-feeds
 wget -N https://github.com/sanskrit-coders/db-interface/raw/master/bin/artifacts/db-interface.jar 
-java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/sa/requestJsons/mahAbhArata-mUla-paThanam.json  --outputPath auto-built-feeds/mahAbhArata-mUla-paThanam.rss
+
+## Sanskrit
+java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/sa/requestJsons/mahAbhArata-mUla-paThanam.json  --outputPath auto-built-feeds/sa/mahAbhArata-mUla-paThanam.rss
+java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/sa/requestJsons/nyAya-shAstra-vidyAdharI.json  --outputPath  auto-built-feeds/sa/nyAya-shAstra-vidyAdharI.rss
+
+### Rare updates
+java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/sa/requestJsons/navya-nyAya-pAThaH-ALvArtaH.json  --outputPath  auto-built-feeds/sa/navya-nyAya-pAThaH-ALvArtaH.rss
+
+## Kannada
 java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/kn/requestJsons/nAgusPodcasts.json  --outputPath  auto-built-feeds/nAgusPodcasts.rss
-java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/sa/requestJsons/nyAya-shAstra-vidyAdharI.json  --outputPath  auto-built-feeds/nyAya-shAstra-vidyAdharI.rss
+
+## English
 java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/en/requestJsons/hindu-sanskrit-lectures-yt-vv.json  --outputPath  auto-built-feeds/hindu-sanskrit-lectures-yt-vv.rss
-java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/sa/requestJsons/SrijanFoundationTalks.json  --outputPath  auto-built-feeds/SrijanFoundationTalks.rss
-curl -X GET "http://54.244.106.57:9090/podcasts/v1/archiveItems/navya-nyAya-pAThaH-ALvArtaH?publisherEmail=podcast-bhaaratii%40googlegroups.com&languageCode=sa&categoriesCsv=Society%20%26%20Culture&imageUrl=https%3A%2F%2Fi.imgur.com%2FdQjPQYi.jpg&isExplicitYesNo=no" -H "accept: application/rss+xml" > auto-built-feeds/navya-nyAya-pAThaH-ALvArtaH.rss
-curl -X GET "http://54.244.106.57:9090/podcasts/v1/archiveItems/Sribhashya-of-Sri-Ramanuja-taught-by-M-A-Alwar?publisherEmail=podcast-bhaaratii%40googlegroups.com&languageCode=ta&categoriesCsv=Society%20%26%20Culture&imageUrl=https%3A%2F%2Fi.imgur.com%2FdQjPQYi.jpg&isExplicitYesNo=no" -H "accept: application/rss+xml" > auto-built-feeds/Sribhashya-of-Sri-Ramanuja-taught-by-M-A-Alwar.rss
+
+### Rare updates
+java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/en/requestJsons/SrijanFoundationTalks.json  --outputPath  auto-built-feeds/SrijanFoundationTalks.rss
+
+## Tamil
+java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/ta/requestJsons/Sribhashya-of-Sri-Ramanuja-taught-by-M-A-Alwar.json  --outputPath  auto-built-feeds/ta/Sribhashya-of-Sri-Ramanuja-taught-by-M-A-Alwar.rss
