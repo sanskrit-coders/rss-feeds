17bd601165e406d15095af6e1d22029e90f251b7
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-09-24 13:31:07 +0530

    CU

--- build_feeds.sh
+++ build_feeds.sh
@@ -17,5 +17,6 @@ wget -N https://github.com/sanskrit-coders/db-interface/raw/master/bin/artifacts
 java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/en/requestJsons/SrijanFoundationTalks.json  --outputPath  auto-built-feeds/en/SrijanFoundationTalks.rss
 java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/en/requestJsons/IndicTodayTalks.json  --outputPath  auto-built-feeds/en/IndicTodayTalks.rss
 java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/en/requestJsons/IndicAcademyTalks.json  --outputPath  auto-built-feeds/en/IndicAcademyTalks.rss
+java -Xmx1G -Xss32m -jar db-interface.jar podcastFromRequest --requestJsonPath feeds/ta/requestJsons/rAmAnuja-dayA-audio.json  --outputPath  auto-built-feeds/ta/rAmAnuja-dayA-audio.rss
 
 ## Tamil
